0327cca9fff24c4b445461a9dc3e947b01fc8dbc
Lit Koder <litkoder@gmail.com>
2019-12-16 08:32:24 +0800

    Add initial files

---
+++ NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+/* Better CSS suppport for browsers, some good defaults */
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function NavBar() {
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="title" color="inherit">
+                        Lit codes
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default NavBar;
--- index.js
+++ index.js
@@ -1,78 +1,21 @@
+/* React and its related libs */
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
+/* Load the whole roboto font, may not be very optimized */
+import 'typeface-roboto';
+/* Better CSS suppport for browsers, some good defaults */
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from './NavBar';
+console.log(NavBar);
 
-const backgroundImage = 'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
-const styles = (theme) => ({
-  background: {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundColor: '#7fc7d9', // Average color of the background image.
-    backgroundPosition: 'center',
-  },
-  button: {
-    minWidth: 200,
-  },
-  h5: {
-    marginBottom: theme.spacing(4),
-    marginTop: theme.spacing(4),
-    [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(10),
-    },
-  },
-  more: {
-    marginTop: theme.spacing(2),
-  },
-});
 function App(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <AppBar position="fixed">
-        <Toolbar className={classes.toolbar}>
-          <div className={classes.left} />
-          <Link
-            variant="h6"
-            underline="none"
-            color="inherit"
-            className={classes.title}
-            href="/"
-          >
-            Home
-          </Link>
-          <div className={classes.right}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              className={classes.rightLink}
-              href="/apps/"
-            >
-              Apps
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              className={clsx(classes.rightLink, classes.linkSecondary)}
-              href="/blog/"
-            >
-              Blog
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <div className={classes.placeholder} />
-    </div>
-  );
+    return (
+        <React.fragment>
+            <CssBaseline />
+            <NavBar />
+        </React.fragment>
+    );
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-const Index = withStyles(styles)(App);
-ReactDOM.render(<Index />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
